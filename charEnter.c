a46a4783386c1cb8f3d292d3268dfd568a507365
ronnieOO700 <63806388+ronnieOO700@users.noreply.github.com>
2020-04-18 21:00:54 +0530

    Update charEnter.c

--- charEnter.c
+++ charEnter.c
@@ -5,6 +5,7 @@ int main()
 	char a,b;
 	FILE *p;
 	p=fopen("test.txt","w");
+	// i am using Dev c++ ver: 4.9.9.2 . you can find your file(created by the code) in documents folder of your pc
 	do
 	{
 		printf("\n enter a character:");
