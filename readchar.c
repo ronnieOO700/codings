5ef07a11c6572408c35f8db27cc21b781f66c733
ronnieOO700 <63806388+ronnieOO700@users.noreply.github.com>
2020-04-18 20:53:15 +0530

    Add files via upload

---
+++ readchar.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+// all Codes are done in Dev C++ version 4.9.9.2
+int main()
+{
+	char a,b;
+	FILE *p;
+	p=fopen("test.txt","r");
+	if(p==NULL)
+	{
+		printf("file Doesn't exist'");
+		exit(0);
+	}
+	while(1)
+	{
+		a=fgetc(p);
+		if(a==EOF)
+		break;
+		else
+		printf("%c",a);
+	}
+	fclose(p);
+	return(0);
+}
